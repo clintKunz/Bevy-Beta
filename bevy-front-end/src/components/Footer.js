5925d6554fede037f37d7242f802635fca924521
Clint Kunz <clintkunz@gmail.com>
2018-11-12 17:46:54 -0700

    style footer

--- bevy-front-end/src/components/Footer.js
+++ bevy-front-end/src/components/Footer.js
@@ -10,7 +10,7 @@ class Footer extends Component {
         <div className='email-signup'>
             <h2>Bevy-Beta</h2>
             <p>Sign up for our newsletter</p>
-            <div className=''>
+            <div className='subscribe'>
                 <input type="email" placeholder='Email Address'/>
                 <div className="btn-subscribe">
                 Subscribe
@@ -18,20 +18,23 @@ class Footer extends Component {
             </div>
             <div className="social-links">twitter, facebook</div>
         </div>
-        <div className="company">
+        <div className="footer-links">
+            <h4>Company</h4>
             <a href="#">Home</a>
             <a href="#">Features</a>
             <a href="#">Pricing</a>
             <a href="#">Why Bevy?</a>
             <a href="#">Sign Up</a>
         </div>
-        <div className="questions">
+        <div className="footer-links">
+            <h4>Questions</h4>
             <a href="#">Origination</a>
             <a href="#">Payments</a>
             <a href="#">Delinquency</a>
             <a href="#">Tax Forms</a>
         </div>
-        <div className="legal">
+        <div className="footer-links">
+            <h4>Legal</h4>
             <a href="#">Privacy Policy</a>
             <a href="#">Terms of Use</a>
         </div>
