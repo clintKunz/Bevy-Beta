295be2c29b27122103b992b1c1eae07c6d9da799
Clint Kunz <clintkunz@gmail.com>
2018-11-14 12:01:52 -0700

    route link home cta component buttons

--- bevy-front-end/src/components/HomePage/HomeCTA.js
+++ bevy-front-end/src/components/HomePage/HomeCTA.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import '../../App.css';
+import { Link } from 'react-router-dom';
 
 //Components
 
@@ -10,8 +11,8 @@ class HomeCTA extends Component {
         <div className='cta-text'>
           <h2>Bevy makes family lending less awkward</h2>
           <p>Customize the terms then let Bevy take care of the rest</p>
-          <div className='btn sign-up'>Sign Up</div>
-          <div className='btn learn-more'>Learn More</div>
+          <Link to='/sign-up' className='btn sign-up'>Sign Up</Link>
+          <Link to='/why-bevy' className='btn learn-more'>Learn More</Link>
         </div>
         <div className='cta-image'>
           <img src="" alt=""/>
