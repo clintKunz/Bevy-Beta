9b141ddec60abfc81857f05a197d6a03ba045d24
Clint Kunz <clintkunz@gmail.com>
2018-11-29 23:23:55 -0700

    add page 8 to the loan input component

---
+++ bevy-front-end/src/components/Loan/Pages/LoanP8.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+//Components
+
+class LoanP8 extends Component {
+  render() {
+    return (
+      <div className=''>
+        <Link to='/loan/page-7'>Back</Link>
+      </div>
+    );
+  }
+}
+
+export default LoanP8;
