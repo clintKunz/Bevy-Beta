1e5752e55583928266df6118e5a3b4c1697957e2
Clint Kunz <clintkunz@gmail.com>
2018-11-29 23:23:09 -0700

    add routes to Loan component

--- bevy-front-end/src/components/Loan/Loan.js
+++ bevy-front-end/src/components/Loan/Loan.js
@@ -5,6 +5,12 @@ import { Route } from 'react-router-dom';
 import Footer from '../Footer';
 import LoanP1 from './Pages/LoanP1';
 import LoanP2 from './Pages/LoanP2';
+import LoanP3 from './Pages/LoanP3';
+import LoanP4 from './Pages/LoanP4';
+import LoanP5 from './Pages/LoanP5';
+import LoanP6 from './Pages/LoanP6';
+import LoanP7 from './Pages/LoanP7';
+import LoanP8 from './Pages/LoanP8';
 
 class Loan extends Component {
   render() {
@@ -17,6 +23,12 @@ class Loan extends Component {
             <div className="input">
                 <Route exact path='/loan'component={LoanP1} />
                 <Route exact path='/loan/page-2' component={LoanP2} />
+                <Route exact path='/loan/page-3' component={LoanP3} />
+                <Route exact path='/loan/page-4' component={LoanP4} />
+                <Route exact path='/loan/page-5' component={LoanP5} />
+                <Route exact path='/loan/page-6' component={LoanP6} />
+                <Route exact path='/loan/page-7' component={LoanP7} />
+                <Route exact path='/loan/page-8' component={LoanP8} />
             </div>
             <div className='form-container'>
                 <p>form...</p>
