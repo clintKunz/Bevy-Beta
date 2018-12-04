a049e3d0f7df4471b61d7788ad84db6ef5319b06
Clint Kunz <clintkunz@gmail.com>
2018-12-03 22:26:31 -0700

    add payment methods and inputs

--- bevy-front-end/src/components/Loan/Pages/LoanP3.js
+++ bevy-front-end/src/components/Loan/Pages/LoanP3.js
@@ -7,6 +7,10 @@ class LoanP3 extends Component {
   render() {
     return (
       <div className=''>
+        <h3>Acceptable Payments</h3>
+        <input type="checkbox"/>Checks
+        <input type="checkbox"/>Venmo/Paypall
+        <br/>
         <Link to='/loan/page-2'>Back</Link>
         <Link to='/loan/page-4'>Next</Link>
       </div>
