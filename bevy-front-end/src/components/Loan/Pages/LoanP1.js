7a2618878c6557b929b4e87a2886b187e1fdfd89
Clint Kunz <clintkunz@gmail.com>
2018-12-03 22:26:01 -0700

    add names and emails and inputs html

--- bevy-front-end/src/components/Loan/Pages/LoanP1.js
+++ bevy-front-end/src/components/Loan/Pages/LoanP1.js
@@ -7,6 +7,14 @@ class LoanP1 extends Component {
     render() {
       return (
         <div className=''>
+          <h3>Lender's Name</h3>
+          <input type="text" placeholder="Name"/>
+          <h4>Lender's Email</h4>
+          <input type="text" placeholder="Email"/>
+          <h3>Borrow's Name</h3>
+          <input type="text" placeholder="Name"/>
+          <h4>Borrow's Email</h4>
+          <input type="text" placeholder="Email"/>
           <Link to='/loan/page-2'>Next</Link>
         </div>
       );
