54e4d2fb9535491634f8bb7d869fddb9000f3c23
Clint Kunz <clintkunz@gmail.com>
2018-12-03 22:26:18 -0700

    add loan terms and inputs

--- bevy-front-end/src/components/Loan/Pages/LoanP2.js
+++ bevy-front-end/src/components/Loan/Pages/LoanP2.js
@@ -7,6 +7,19 @@ class LoanP2 extends Component {
   render() {
     return (
       <div className=''>
+        <h3>Loan Amount</h3>
+        <input type="number" placeholder="$Amount"/>
+        <h4>Payback Start Date</h4>
+        <input type="date"/>
+        <h4>Loan Length</h4>
+        <select>
+          <option value="months">Months</option>
+          <option value="years">Years</option>
+        </select>
+        <input type="number" placeholder="Length"/>
+        <h4>Interest Rate</h4>
+        <input type="number" placeholder="5%"/>
+        <br/>
         <Link to='/loan'>Back</Link>
         <Link to='/loan/page-3'>Next</Link>
       </div>
