b635c3c2b5284cec936c0e536c5c7200e0f5fb0d
Clint Kunz <clintkunz@gmail.com>
2018-12-13 18:04:19 -0700

    add Form component

---
+++ bevy-front-end/src/components/Loan/Form/Form.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+//Components
+
+class Form extends Component {
+    render() {
+      return (
+        <div className='form'>
+            
+        </div>
+      );
+    }
+  }
+  
+  export default Form;
